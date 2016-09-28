d5b4fa0667f999d2f9a9a43fc06cf607b73343f0
Tw31n <alexander.tw31n@gmail.com>
2016-09-28 17:56:18 +0300

    Add uglifyjs

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -17,4 +17,10 @@ module.exports = {
             }
         ]
     }
+    ,
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
+    ]
 }

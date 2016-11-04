08ea44740ea037848253df307b36b595010b1594
Tw31n <alexander.tw31n@gmail.com>
2016-11-04 13:53:17 +0200

    Show list of heroes

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 @Component({
     selector: 'angular2-app',
-    template: '<h1>Angular 2</h1>'
+    templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+
+export class AppComponent {
+    heroes = [
+        new Hero(1, 'Superman'),
+        new Hero(2, 'Batman'),
+        new Hero(3, 'Spiderman')
+    ];
+
+    selectedHero = this.heroes[0];
+}

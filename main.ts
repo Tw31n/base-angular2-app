283972bf79a8e952ae00ab5b3155b49b744c5a65
Tw31n <alexander.tw31n@gmail.com>
2016-11-07 19:27:11 +0200

    Rename application's root folder to app/ (guide style recommendation)

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './src/app.module';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);

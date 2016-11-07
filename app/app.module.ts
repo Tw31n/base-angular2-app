55f5330a551a67caf59da2f096e65f49f81d0608
Tw31n <alexander.tw31n@gmail.com>
2016-11-07 23:19:56 +0200

    Create CoreModule for single-use components and services

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component';
+import { CoreModule }   from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     imports: [
         BrowserModule,
+        CoreModule,
         SharedModule
     ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 })
 
-export class AppModule { }
+export class AppModule {}
